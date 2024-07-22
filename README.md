ccdd10fd59b1377146241ead0b9d66a94524923b
Hermann Stamm-Wilbrandt <hermann@7600x.fritz.box>
2024-07-22 20:04:50 +0200

    21 real degree-3 type insertions from the technical report

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://gist.github.com/Hermann-SW/99d151a273d290ee0d843c79b2da26a8#file-straigh
 
 For now algorithm will be created with CGAL which includes Boost graph library, and allows to display intermediate graphs using ```CGAL::draw()``` while developing code, for visual debugging. Call to that function is blocking, but ```fork()``` with ```kill()``` of child executing ```CGAL::draw()``` even allows for automatic animation sequence. Only during CGAL code development incremental straingt line drawing coordinates will be determined per [chapter 7](https://raw.githubusercontent.com/Hermann-SW/randomgraph/4bb2caf643b0016c32820349eb3ea772c53fb529/IAI-TR-93-10.ps.pdf#page=10) of the 1993 technical report. Later better ```chrobak_payne_straight_line_drawing()``` will be used because of its much better minimal angle guarantee for generated straight line drawing.
 
-Here is current ```CGAL::draw()``` demo sofar, with new function ```void draw_timeout(const Polyhedron& P, int ms)```, called with 1200ms in [build+run](build+run):  
+Here is current ```CGAL::draw()``` demo sofar, with new function ```void draw_timeout(const Polyhedron& P, int ms)```, called with 400ms in [build+run](build+run). Repeated degree-3 insertions of the technical report, not random yet:  
 ![res/demo.cpp.anim.gif](res/demo.cpp.anim.gif)
 
 [build+run](build+run) and [really_clean](really_clean) script do what they state.
--- demo.cpp
+++ demo.cpp
@@ -15,70 +15,62 @@ typedef Polyhedron::Halfedge_handle                          Halfedge_handle;
 
 // from: examples/Polyhedron/polyhedron_prog_subdiv_with_boundary.cpp#L25-L37
 //
-void create_center_vertex( Polyhedron& P, Facet_iterator f) {
-    Vector_3 vec( 0.0, 0.0, 0.0);
+void create_center_vertex(Polyhedron *P, Facet_iterator f) {
+    Vector_3 vec(0.0, 0.0, 0.0);
     std::size_t order = 0;
     HF_circulator h = f->facet_begin();
     do {
-        vec = vec + ( h->vertex()->point() - CGAL::ORIGIN);
-        ++ order;
+        vec = vec + (h->vertex()->point() - CGAL::ORIGIN);
+        ++order;
     } while ( ++h != f->facet_begin());
-    assert( order >= 3); // guaranteed by definition of polyhedron
+    assert(order >= 3);
     Point_3 center =  CGAL::ORIGIN + (vec / static_cast<double>(order));
-    Halfedge_handle new_center = P.create_center_vertex( f->halfedge());
+    Halfedge_handle new_center = P->create_center_vertex(f->halfedge());
     new_center->vertex()->point() = center;
 }
 
-void draw_timeout(const Polyhedron& P, int ms) {
-    pid_t pid;
-
-    pid = fork();
-    if (pid == -1)
-    {
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        CGAL::draw(P);
-    }
-    else if (pid > 0)
-    {
-        usleep(1000*ms);
-        kill(pid, SIGKILL);
+void draw_timeout(const Polyhedron* P, int ms) {
+    pid_t pid = fork();
+           if (pid == -1) { exit(1);
+    } else if  (pid == 0) { CGAL::draw(*P);
+    } else if   (pid > 0) { usleep(1000*ms); kill(pid, SIGKILL);
     }
 }
 
 int main(int argc, char *argv[]) {
     int ms = (argc < 2) ? 1000 : atoi(argv[1]);
 
-    Point_3 p( 0.0, 0.0, 0.0 );
-    Point_3 q( 0.0, 1.0, 0.0 );
-    Point_3 r( 1.0, 0.0, 0.0 );
-    Point_3 s( 0.4, 0.4, 0.0 );
+    Point_3 p(0.0, 0.0, 0.0);
+    Point_3 q(0.5, sqrt(3.0)/2.0, 0.0);
+    Point_3 r(1.0, 0.0, 0.0);
+    Point_3 s(0.5, sqrt(3.0)/6.0, 0.0);
 
     Polyhedron P;
-    boost::graph_traits<Polyhedron>::halfedge_descriptor h = P.make_tetrahedron( s, p, q, r );
+    boost::graph_traits<Polyhedron>::halfedge_descriptor hn;
+    boost::graph_traits<Polyhedron>::halfedge_descriptor h
+        = P.make_tetrahedron(s, p, q, r);
 
-    CGAL::IO::set_ascii_mode( std::cout );
-    
+    CGAL::IO::set_ascii_mode(std::cout);
+
     for ( Vertex_iterator v = P.vertices_begin(); v != P.vertices_end(); ++v )
         std::cout << v->point() << std::endl;
 
     std::cout << "=====" << std::endl;
 
-    draw_timeout(P, ms);
+    draw_timeout(&P, ms);
 
 //    P.join_facet(h);
 //    P.split_edge(h);
 
-    create_center_vertex(P,h->facet());
+    for (int i = 0; i < 3 + 6+12; ++i) {
+        // hn=h->opposite()->prev();
+        hn = h->next()->opposite();
+        create_center_vertex(&P, h->facet());
 
-    std::cout << "=====" << std::endl;
+        if (i == 20) { CGAL::draw(P); } else { draw_timeout(&P, ms); }
 
-    for ( Vertex_iterator v = P.vertices_begin(); v != P.vertices_end(); ++v )
-        std::cout << v->point() << std::endl;
-
-    draw_timeout(P, ms);
+        h = hn;
+    }
 
     return 0;
 }
