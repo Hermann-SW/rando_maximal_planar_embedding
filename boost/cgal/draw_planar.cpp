41bbfdcae076034e6109efd72b9bd51258187c4e
Hermann Stamm-Wilbrandt <hermann@7600x.fritz.box>
2024-07-30 18:08:56 +0200

    initial Qt5 only draw_planar

---
+++ boost/cgal/draw_planar.cpp
@@ -0,0 +1,29 @@
+#include "draw_planar.hpp"
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <fstream>
+#include <cassert>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel  Kernel;
+typedef CGAL::Polyhedron_3<Kernel>                       Polyhedron;
+typedef Kernel::Point_3                                     Point_3;
+
+int main(int argc, char* argv[]) {
+  assert(argc == 2);
+
+  Polyhedron P;
+  std::ifstream in1(argv[1]);
+  in1 >> P;
+
+  for (auto it = P.points_begin(); it != P.points_end(); ++it)
+      if (abs(it->z()) > 1e-6) {
+          std::cout << "not all points z-coordinate are 0" << std::endl;
+          exit(1);
+      }
+
+  *P.points().first = Point_3(0.0, 0.0, -1.0);
+  CGAL::draw(P);
+
+  return EXIT_SUCCESS;
+}
